c77aa76ccc42bd8ebc0ae50a7ed88c677b63060d
isakbosman <isakbosman@users.noreply.github.com>
2020-07-21 17:57:11 -0700

    Updated git and files

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,18 +1,16 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 # Exit in case of error
 set -e
 
-DOMAIN=backend \
-SMTP_HOST="" \
-TRAEFIK_PUBLIC_NETWORK_IS_EXTERNAL=false \
-INSTALL_DEV=true \
-docker-compose \
--f docker-compose.yml \
-config > docker-stack.yml
+# Run this from the root of the project
 
-docker-compose -f docker-stack.yml build
-docker-compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
-docker-compose -f docker-stack.yml up -d
-docker-compose -f docker-stack.yml exec -T backend bash /app/tests-start.sh "$@"
-docker-compose -f docker-stack.yml down -v --remove-orphans
+rm -rf ./testing-project
+
+cookiecutter --no-input -f ./ project_name="Testing Project"
+
+cd ./testing-project
+
+bash ./scripts/test.sh "$@"
+
+cd ../
